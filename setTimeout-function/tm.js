e131ffa901f4526479aa06400da450508703a40f
receptan <testertaner@yandex.com>
2019-04-25 12:11:37 +0300

    setTimeOut

---
+++ setTimeout-function/tm.js
@@ -0,0 +1,13 @@
+
+/* 1. Kullanım start */
+setTimeout(function () {
+    console.log("Selam Nasılsın?");
+}, 5000);
+/* 1. Kullanım end */
+/* 2. Kullanım start */
+var hi = function () {
+    console.log("Merhaba Dostum");
+};
+
+setTimeout(hi,2000);
+/* 2. Kullanım end */
