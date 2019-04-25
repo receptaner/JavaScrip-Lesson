e709e5b3a78681840e97cbff5ab19452099cd9f4
receptan <testertaner@yandex.com>
2019-04-25 11:04:59 +0300

    variable

---
+++ variable/degiskenler.js
@@ -0,0 +1,8 @@
+var hello = "Merhaba Ben Var İle Yazıldım";
+console.log(hello);
+var userDataCount = 2;
+console.log(userDataCount);
+var data = "JavaScript Öğreniyorum", atad = "Öğreniyorum JavaScript";
+console.log(data, atad);
+var camelCase = "camelCase ilk harfi küçük ikinci kelimenin ilk harfi büük şekilde yazılır";
+console.log(camelCase);
