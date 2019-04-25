6c56c1612d77fa5500fdba5c577b358e263f49d4
receptan <testertaner@yandex.com>
2019-04-25 12:03:35 +0300

    callBack Function

---
+++ functionS/fun.js
@@ -0,0 +1,11 @@
+function trueadd(deger = -2) {
+    console.log(deger+2);
+}
+
+trueadd(654);
+
+function nameSurname(name, surname) {
+    return name +" "+surname;
+}
+
+console.log(nameSurname("recep taner","sönmez"));
